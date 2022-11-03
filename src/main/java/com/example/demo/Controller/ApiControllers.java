a0cdf3fe9cb2e992e7e1ac3f9c55ccecf92d1860
Khaled Taha <khaledtaha1022001@gmail.com>
2022-11-03 11:40:46 +0200

    last changes

--- src/main/java/com/example/demo/Controller/ApiControllers.java
+++ src/main/java/com/example/demo/Controller/ApiControllers.java
@@ -12,32 +12,37 @@ import java.util.Optional;
 @RestController
 public class ApiControllers {
     @Autowired
-private UserRepo userRepo;
+    private UserRepo userRepo;
+
     @GetMapping(value = "/")
-    public String getPage(){
-    return "Welcome";
+    public String getPage() {
+        return "Welcome";
     }
+
     @GetMapping(value = "/users")
-    public List<User> getUsers(){
+    public List<User> getUsers() {
         return userRepo.findAll();
     }
-    @PostMapping(value="/save")
-    public String saveUser(@RequestBody User user){
+
+    @PostMapping(value = "/save")
+    public String saveUser(@RequestBody User user) {
         userRepo.save(user);
         return "Saved ..";
     }
-    @PutMapping(value="/update/{id}")
-    public String updateUser(@PathVariable long id,@RequestBody User user){
-      User updatedUser = userRepo.findById(id).get();
-      updatedUser.setFirstName(user.getFirstName());
+
+    @PutMapping(value = "/update/{id}")
+    public String updateUser(@PathVariable long id, @RequestBody User user) {
+        User updatedUser = userRepo.findById(id).get();
+        updatedUser.setFirstName(user.getFirstName());
         updatedUser.setLastName(user.getLastName());
         updatedUser.setAge(user.getAge());
         updatedUser.setOccupation(user.getOccupation());
         userRepo.save(updatedUser);
         return "Updated ..";
     }
+
     @DeleteMapping(value = "/delete/{id}")
-    public String deleteUser(@PathVariable long id ){
+    public String deleteUser(@PathVariable long id) {
         User deleteUser = userRepo.findById(id).get();
         userRepo.delete(deleteUser);
         return "Deleted Successfully ";
